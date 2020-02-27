f74033179b8ca484a3c7af4df3067eed578462a5
nolim1t <hello@nolim1t.co>
2020-02-27 12:49:37 +0700

    Add real docker-compose file

--- stage2/04-docker-compose/01-run.sh
+++ stage2/04-docker-compose/01-run.sh
@@ -4,7 +4,11 @@ on_chroot << EOF
 pip3 install docker-compose
 EOF
 
-# Maybe generate a compose file to use
+# Maybe generate docker-compose file so we can use it
+chmod 644 files/docker-compose.yml
+cp files/docker-compose.yml   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/docker-compose.yml
+
+
 echo "Docker stuff installed" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/docker-compose.txt
 
 
