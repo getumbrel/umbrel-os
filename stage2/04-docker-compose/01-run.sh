b66447bcefe69c273fc66d1d0e36062d49969702
nolim1t <hello@nolim1t.co>
2020-02-27 16:25:21 +0700

    Add unlock scripts into this

--- stage2/04-docker-compose/01-run.sh
+++ stage2/04-docker-compose/01-run.sh
@@ -7,6 +7,8 @@ EOF
 # Maybe generate docker-compose file so we can use it
 chmod 644 files/docker-compose.yml
 cp files/docker-compose.yml   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/docker-compose.yml
+cp files/umbrel-createwallet.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-createwallet.py
+cp files/umbrel-unlock.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-unlock.py
 
 
 echo "Docker stuff installed" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/docker-compose.txt
