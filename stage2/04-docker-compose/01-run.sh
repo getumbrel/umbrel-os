8bcd4f0764d6f9ee7e14b475f33518465997cdf9
nolim1t <hello@nolim1t.co>
2020-02-29 17:15:36 +0700

    Run symlinking within the chroot

--- stage2/04-docker-compose/01-run.sh
+++ stage2/04-docker-compose/01-run.sh
@@ -13,32 +13,37 @@ cp files/umbrel-createwallet.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-cre
 cp files/umbrel-unlock.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-unlock.py
 
 # Docker compose service
-mkdir -p ${ROOTFS_DIR}/etc/init.d
-mkdir -p ${ROOTFS_DIR}/etc/rc2.d
-mkdir -p ${ROOTFS_DIR}/etc/rc3.d
-mkdir -p ${ROOTFS_DIR}/etc/rc4.d
-mkdir -p ${ROOTFS_DIR}/etc/rc5.d
-mkdir -p ${ROOTFS_DIR}/etc/rc0.d
-mkdir -p ${ROOTFS_DIR}/etc/rc1.d
-mkdir -p ${ROOTFS_DIR}/etc/rc6.d
+on_chroot << EOF
+mkdir -p /etc/init.d
+mkdir -p /etc/rc2.d
+mkdir -p /etc/rc3.d
+mkdir -p /etc/rc4.d
+mkdir -p /etc/rc5.d
+mkdir -p /etc/rc0.d
+mkdir -p /etc/rc1.d
+mkdir -p /etc/rc6.d
+EOF
 
 echo "Copying the compose service to rootfs (etc/init.d)"
 cp files/compose-service ${ROOTFS_DIR}/etc/init.d/umbrelbox
 cp files/compose-service ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrelbox
-cd ${ROOTFS_DIR}/etc/rc2.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox S01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc3.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox S01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc4.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox S01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc5.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox S01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc0.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox K01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc1.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox K01umbrelbox
-cd ${ROOTFS_DIR}/etc/rc6.d
-ln -s ${ROOTFS_DIR}/etc/init.d/umbrelbox K01umbrelbox
+
+on_chroot << EOF
+cd /etc/rc2.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc3.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc4.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc5.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc0.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+cd /etc/rc1.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+cd /etc/rc6.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+EOF
 
 echo "Docker stuff installed" >> $ROOTFS_DIR/home/$FIRST_USER_NAME/docker-compose.txt
 
