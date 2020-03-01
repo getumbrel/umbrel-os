66e209169b497f5955e7072ec4e2beea50a2027b
nolim1t <hello@nolim1t.co>
2020-03-01 15:14:28 +0700

    Add todo

--- stage2/08-configure-bitcoind/01-run.sh
+++ stage2/08-configure-bitcoind/01-run.sh
@@ -1,3 +1,5 @@
+# TODO: configure username and password (internally)
+
 chmod 644 files/bitcoin.conf
 mkdir ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bitcoin
 cp files/bitcoin.conf   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bitcoin/bitcoin.conf
