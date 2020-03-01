3715bccfb1fb4352d1e5f991fcc08fc1a55018f4
nolim1t <hello@nolim1t.co>
2020-03-01 14:27:21 +0700

    Fetch ssh keys from github if GITHUB_USERNAME is selected

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -25,6 +25,13 @@ fi
 systemctl enable regenerate_ssh_host_keys
 EOF
 
+if [ ! -z ${GITHUB_USERNAME} ]; then
+    echo "Setting up authorized_keys file"
+    cd $ROOTFS_DIR/home/$FIRST_USER_NAME
+    mkdir -p .ssh
+    curl "https://github.com/${GITHUB_USERNAME}.keys" > authorized_keys
+fi
+
 if [ "${USE_QEMU}" = "1" ]; then
 	echo "enter QEMU mode"
 	install -m 644 files/90-qemu.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
