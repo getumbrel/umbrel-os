83a9a1ac7834df31b2efce5545ef7d7af507c37b
nolim1t <hello@nolim1t.co>
2020-03-02 23:09:09 +0700

    Fix up TORRC copy

--- stage2/09-configure-tor/01-run.sh
+++ stage2/09-configure-tor/01-run.sh
@@ -1,5 +1,6 @@
 # TODO: configure username and password (internally)
 
 chmod 644 files/torrc
-mkdir -p ${ROOTFS_DIR}/etc/tor
-cp files/torrc   ${ROOTFS_DIR}/etc/torrc
+mkdir -p ${ROOTFS_DIR}/etc/tor/
+echo "Overriding TORRC"
+cp files/torrc   ${ROOTFS_DIR}/etc/tor/torrc
