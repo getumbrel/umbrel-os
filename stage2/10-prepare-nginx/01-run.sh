a48d1814312829e83b7eabe26841062ec2324ba4
nolim1t <hello@nolim1t.co>
2020-03-03 11:28:30 +0700

    fix up chowm command

--- stage2/10-prepare-nginx/01-run.sh
+++ stage2/10-prepare-nginx/01-run.sh
@@ -12,6 +12,6 @@ cp -fr files/conf.d   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/nginx
 
 echo "Fix up permissions of all config files"
 on_chroot << EOF
-chown -r ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 EOF
 
