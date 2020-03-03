9097889d17db94332e873cd38c09434981ad098d
nolim1t <hello@nolim1t.co>
2020-03-03 10:48:30 +0700

    Add rpcauth.py to /Users/barryteoh/bin

--- stage2/05-prepare-home-dir/01-run.sh
+++ stage2/05-prepare-home-dir/01-run.sh
@@ -5,5 +5,6 @@ touch $ROOTFS_DIR/home/$FIRST_USER_NAME/.save_password
 echo "Making Secrets directory"
 mkdir -p $ROOTFS_DIR/home/$FIRST_USER_NAME/secrets
 
-
+echo "Executablesi directory"
+mkdir -p $ROOTFS_DIR/home/$FIRST_USER_NAME/bin
 
--- stage2/08-configure-bitcoind/01-run.sh
+++ stage2/08-configure-bitcoind/01-run.sh
@@ -4,3 +4,7 @@ chmod 644 files/bitcoin.conf
 mkdir ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bitcoin
 cp files/bitcoin.conf   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bitcoin/bitcoin.conf
 
+echo "Downloading password utility"
+cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin
+curl "https://raw.githubusercontent.com/bitcoin/bitcoin/master/share/rpcauth/rpcauth.py" 2>/dev/null 1>rpcauth.py
+chmod 755 rpcauth.py
