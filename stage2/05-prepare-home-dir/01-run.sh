5dd85e62a46771bc9ec02acaad1da5205fa04b5e
nolim1t <hello@nolim1t.co>
2020-03-06 12:17:27 +0700

    Remove secrets creation as that should already be done

--- stage2/05-prepare-home-dir/01-run.sh
+++ stage2/05-prepare-home-dir/01-run.sh
@@ -2,9 +2,6 @@
 # We will remove this later when a more suitable secure system is finalized.
 touch $ROOTFS_DIR/home/$FIRST_USER_NAME/.save_password
 
-echo "Making Secrets directory"
-mkdir -p $ROOTFS_DIR/home/$FIRST_USER_NAME/secrets
-
-echo "Executablesi directory"
+echo "Executables directory"
 mkdir -p $ROOTFS_DIR/home/$FIRST_USER_NAME/bin
 
