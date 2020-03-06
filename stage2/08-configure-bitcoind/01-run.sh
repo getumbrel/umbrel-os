5de2011e5f8510c2e3c8ee9285edfb3d3e936899
nolim1t <hello@nolim1t.co>
2020-03-06 12:23:43 +0700

    Fix permissions of bitcoind

--- stage2/08-configure-bitcoind/01-run.sh
+++ stage2/08-configure-bitcoind/01-run.sh
@@ -8,3 +8,7 @@ echo "Downloading password utility"
 cd ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/bin
 curl "https://raw.githubusercontent.com/bitcoin/bitcoin/master/share/rpcauth/rpcauth.py" 2>/dev/null 1>rpcauth.py
 chmod 755 rpcauth.py
+
+on_chroot << EOF
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+EOF
