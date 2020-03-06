a329a5cbfcc577179960339088a235508fda68e3
nolim1t <hello@nolim1t.co>
2020-03-06 12:21:59 +0700

    lnd.conf overrwrite

--- stage2/07-configure-LND/01-run.sh
+++ stage2/07-configure-LND/01-run.sh
@@ -1,4 +1,6 @@
 chmod 644 files/lnd.conf
-mkdir ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lnd
+echo "Copying lnd.conf to overwrite the existing LND.conf"
 cp files/lnd.conf   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lnd/lnd.conf
-
+on_chroot << EOF
+chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
+EOF
