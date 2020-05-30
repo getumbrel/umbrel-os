60b47eb42618c28dce689b79371e2f75ac49caeb
nolim1t <hello@nolim1t.co>
2020-05-30 17:00:13 +0700

    these guys are flipped

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -13,6 +13,42 @@ install -m 644 files/console-setup   	"${ROOTFS_DIR}/etc/default/"
 
 install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
 
+# Maybe generate docker-compose file so we can use it
+chmod 755 files/compose-service
+
+# Docker compose service
+on_chroot << EOF
+mkdir -p /etc/init.d
+mkdir -p /etc/rc2.d
+mkdir -p /etc/rc3.d
+mkdir -p /etc/rc4.d
+mkdir -p /etc/rc5.d
+mkdir -p /etc/rc0.d
+mkdir -p /etc/rc1.d
+mkdir -p /etc/rc6.d
+EOF
+
+echo "Copying the compose service to rootfs (etc/init.d)"
+cp files/compose-service ${ROOTFS_DIR}/etc/init.d/umbrelbox
+
+# Install umbrelbox into runlevels
+on_chroot << EOF
+cd /etc/rc2.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc3.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc4.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc5.d
+ln -s /etc/init.d/umbrelbox S01umbrelbox
+cd /etc/rc0.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+cd /etc/rc1.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+cd /etc/rc6.d
+ln -s /etc/init.d/umbrelbox K01umbrelbox
+EOF
+
 on_chroot << EOF
 systemctl disable hwclock.sh
 systemctl disable nfs-common
