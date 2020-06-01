7b274ee8286ac83d0ff88b66f060d03164b617d4
nolim1t <hello@nolim1t.co>
2020-06-01 14:59:44 +0700

    Remove the create wallet and unlock scripts as we don't need them

--- stage2/04-docker-compose/01-run.sh
+++ stage2/04-docker-compose/01-run.sh
@@ -12,10 +12,6 @@ EOF
 # Maybe generate docker-compose file so we can use it
 chmod 755 files/compose-service
 
-# These can be probabably removed
-cp files/umbrel-createwallet.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-createwallet.py
-cp files/umbrel-unlock.py ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel-unlock.py
-
 # Docker compose service
 on_chroot << EOF
 mkdir -p /etc/init.d
