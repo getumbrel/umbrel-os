748284a04fe7493dd7ccf8dcb50b733c845f4396
nolim1t <hello@nolim1t.co>
2020-06-10 21:48:18 +0700

    Remove LND overwritten files

--- stage2/07-configure-LND/01-run.sh
+++
@@ -1,6 +0,0 @@
-chmod 644 files/lnd.conf
-echo "Copying lnd.conf to overwrite the existing LND.conf"
-cp files/lnd.conf   ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/lnd/lnd.conf
-on_chroot << EOF
-chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
-EOF
