2fa3dbfd4ea302331b210443e2623f6076ebc4c9
Mayank <mayankchhabra9@gmail.com>
2020-06-14 11:12:26 +0530

    Fix https

--- stage2/09-install-mender/00-run.sh
+++ stage2/09-install-mender/00-run.sh
@@ -3,6 +3,12 @@
 echo "Installing mender"
 
 on_chroot << EOF
-wget -q ttps://d1b0l86ne08fsf.cloudfront.net/2.3.0b1/dist-packages/debian/armhf/mender-client_2.3.0b1-1_armhf.deb
+wget -q https://d1b0l86ne08fsf.cloudfront.net/2.3.0b1/dist-packages/debian/armhf/mender-client_2.3.0b1-1_armhf.deb
 DEBIAN_FRONTEND=noninteractive dpkg -i mender-client_2.3.0b1-1_armhf.deb
+EOF
+
+echo "Installed mender, now setting it up"
+
+on_chroot << EOF
+mender setup --device-type "raspberrypi4" --server-ip "54.159.58.48" --demo
 EOF
