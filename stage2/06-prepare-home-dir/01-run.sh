bdf538644d4e0e741e3e28453daf6930b2727753
Mayank <mayankchhabra9@gmail.com>
2020-07-01 11:05:19 +0530

    Monitor for OTA signal

--- stage2/06-prepare-home-dir/01-run.sh
+++ stage2/06-prepare-home-dir/01-run.sh
@@ -2,6 +2,3 @@
 # We will remove this later when a more suitable secure system is finalized.
 touch $ROOTFS_DIR/home/$FIRST_USER_NAME/.save_password
 
-echo "Executables directory"
-mkdir -p $ROOTFS_DIR/home/$FIRST_USER_NAME/bin
-
