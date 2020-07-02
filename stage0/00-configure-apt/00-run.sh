c9bc3621f162c102f4762e33b4c19f033f0b35b8
Mayank <mayankchhabra9@gmail.com>
2020-07-02 20:26:39 +0530

    Install go with apt

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -17,14 +17,6 @@ apt-get dist-upgrade -y
 EOF
 
 echo "Installing Go"
-cd ~
-curl -O "https://golang.org/dl/go1.14.4.linux-amd64.tar.gz"
-tar xvf go1.14.4.linux-amd64.tar.gz
-sudo chown -R root:root ./go
-sudo mv go /usr/local
-export GOPATH=$HOME/work
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-
 go version
 echo "Go installed, now downloading images"
 
