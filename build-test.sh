09b2714fb8ca2f8241a7735d9179c02e6e806249
Mayank <mayankchhabra9@gmail.com>
2020-07-03 12:48:34 +0530

    Pull one by one

--- build-test.sh
+++ build-test.sh
@@ -2,7 +2,9 @@ docker --version
 wget -q "https://raw.githubusercontent.com/getumbrel/umbrel-compose/master/docker-compose.yml"
 IMAGES=$(grep '^\s*image' docker-compose.yml | sed 's/image://' | sed 's/\"//g' | sed '/^$/d;s/[[:blank:]]//g' | sort | uniq)
 echo "List of images to download: $IMAGES"
-docker pull --platform=linux/arm/v7 $IMAGES
+while IFS= read -r image; do
+    docker pull --platform=linux/arm/v7 $image
+done <<< "$IMAGES"
 docker images 
 docker save $IMAGES -o umbrel-docker-images.tar
 ls -al
