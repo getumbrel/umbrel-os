9f1cd156304c8104c21a0cdc99914241a7da3422
Mayank <mayankchhabra9@gmail.com>
2020-07-05 09:26:44 +0530

    See what's installed

--- build-test.sh
+++ build-test.sh
@@ -1,24 +1,26 @@
 #!/bin/bash -e
 set -x
 
-wget -q "https://raw.githubusercontent.com/getumbrel/umbrel-compose/master/docker-compose.yml"
-IMAGES=$(grep '^\s*image' docker-compose.yml | sed 's/image://' | sed 's/\"//g' | sed '/^$/d;s/[[:blank:]]//g' | sort | uniq)
-echo "List of images to download: $IMAGES"
-
-mkdir dockerpi
-echo "Running Docker Pi"
-docker run --name dockerpi -v dockerpi:/sdcard lukechilds/dockerpi 
 docker ps
-echo "Downloading install script"
-docker exec -d dockerpi curl -fsSL https://get.docker.com -o docker-install.sh
-echo "Running install script"
-docker exec -d dockerpi ./docker-install.sh
-echo "Pulling images in docker"
-while IFS= read -r image; do
-    docker exec -d docker pull --platform=linux/arm/v7 $image
-done <<< "$IMAGES"
-ls dockerpi
-ls dockerpi/var/lib/tor
+
+# wget -q "https://raw.githubusercontent.com/getumbrel/umbrel-compose/master/docker-compose.yml"
+# IMAGES=$(grep '^\s*image' docker-compose.yml | sed 's/image://' | sed 's/\"//g' | sed '/^$/d;s/[[:blank:]]//g' | sort | uniq)
+# echo "List of images to download: $IMAGES"
+
+# mkdir dockerpi
+# echo "Running Docker Pi"
+# docker run --name dockerpi -v dockerpi:/sdcard lukechilds/dockerpi 
+# docker ps
+# echo "Downloading install script"
+# docker exec -d dockerpi curl -fsSL https://get.docker.com -o docker-install.sh
+# echo "Running install script"
+# docker exec -d dockerpi ./docker-install.sh
+# echo "Pulling images in docker"
+# while IFS= read -r image; do
+#     docker exec -d docker pull --platform=linux/arm/v7 $image
+# done <<< "$IMAGES"
+# ls dockerpi
+# ls dockerpi/var/lib/tor
 # docker save $IMAGES -o umbrel-docker-images.tar
 # du -h umbrel-docker-images.tar
 # docker save $IMAGES | gzip > umbrel-docker-images.tar.gz 
