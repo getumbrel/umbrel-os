c003bb9418612ea8f9be0819be6d059cf5c53962
Mayank <mayankchhabra9@gmail.com>
2020-07-05 09:47:41 +0530

    Detached

--- build-test.sh
+++ build-test.sh
@@ -6,7 +6,7 @@ uname -m
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
 
 mkdir docker-dir
-docker run --rm -t --name dockerdebian -v docker-dir:/var/lib/docker multiarch/debian-debootstrap:armhf-buster-slim bash
+docker run --rm -d --name dockerdebian -v docker-dir:/var/lib/docker multiarch/debian-debootstrap:armhf-buster-slim bash
 docker ps
 docker exec -t dockerdebian uname -m
 
