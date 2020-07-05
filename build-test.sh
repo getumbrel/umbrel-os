6eba4f6f5d3e56b6fb2e7aef1e362717a594638a
Mayank <mayankchhabra9@gmail.com>
2020-07-05 12:03:48 +0530

    Remove dind

--- build-test.sh
+++ build-test.sh
@@ -6,7 +6,7 @@ uname -m
 echo "Removing Docker"
 docker ps
 docker images -a
-du -h /var/lib/docker/overlay2
+du -sh /var/lib/docker/overlay2
 sudo systemctl stop docker
 sudo apt-get purge docker-ce docker-ce-cli containerd.io moby-engine moby-cli
 sudo rm -rf /var/lib/docker
@@ -26,12 +26,12 @@ IMAGES=$(grep '^\s*image' docker-compose.yml | sed 's/image://' | sed 's/\"//g' 
 
 echo "Pulling images in docker"
 while IFS= read -r image; do
-    docker run --rm -t -v docker-dir:/var/lib/docker docker:stable-dind docker pull --platform=linux/arm/v7 $image
+    docker pull --platform=linux/arm/v7 $image
 done <<< "$IMAGES"
 docker images -a
 
 
-du -h /var/lib/docker/overlay2
+du -sh /var/lib/docker/overlay2
 # ls dockerpi
 # ls dockerpi/var/lib/tor
 # docker save $IMAGES -o umbrel-docker-images.tar
