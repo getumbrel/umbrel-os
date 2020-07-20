cb3cf60dcd621da8e00d034427f2e23ffa4d4f77
Luke Childs <lukechilds123@gmail.com>
2020-07-20 23:09:51 +0700

    Add Pipfile and Pipfile.lock to prevent dependency backdoor

--- stage2/06-connection-details/01-run.sh
+++ stage2/06-connection-details/01-run.sh
@@ -2,6 +2,13 @@
 
 echo "Installing connection-details and dependencies"
 
-on_chroot pip3 install qrcode
+cp files/Pipfile* ${ROOTFS_DIR}/
+
+on_chroot << EOF
+PIPENV_PIPFILE="/Pipfile" pipenv --python "$(which python3)" install --system
+EOF
+
+rm ${ROOTFS_DIR}/Pipfile*
+
 chmod +x files/connection-details
 cp files/connection-details ${ROOTFS_DIR}/usr/local/bin/connection-details
