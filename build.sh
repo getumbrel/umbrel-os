2072ade2a488ae7ab8f6a92933d263977d6d5fd5
Mayank <mayankchhabra9@gmail.com>
2020-07-24 10:30:52 +0530

    Debug 2

--- build.sh
+++ build.sh
@@ -207,7 +207,7 @@ source "${SCRIPT_DIR}/common"
 # shellcheck source=scripts/dependencies_check
 source "${SCRIPT_DIR}/dependencies_check"
 
-dependencies_check "${BASE_DIR}/depends"
+# dependencies_check "${BASE_DIR}/depends"
 
 #check username is valid
 if [[ ! "$FIRST_USER_NAME" =~ ^[a-z][-a-z0-9_]*$ ]]; then
