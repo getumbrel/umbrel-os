675d2e36f86bc3c4699ffca5a3ee14c252ffc46c
Luke Childs <lukechilds123@gmail.com>
2020-07-24 15:38:16 +0700

    Quote variables

--- stage2/06-connection-details/01-run.sh
+++ stage2/06-connection-details/01-run.sh
@@ -2,14 +2,14 @@
 
 echo "Installing connection-details and dependencies"
 
-cp files/Pipfile* ${ROOTFS_DIR}/
+cp files/Pipfile* "${ROOTFS_DIR}"/
 
 on_chroot << EOF
 pip3 install qrcode
 EOF
 # PIPENV_PIPFILE="/Pipfile" pipenv --python "$(which python3)" install --system
 
-rm ${ROOTFS_DIR}/Pipfile*
+rm "${ROOTFS_DIR}"/Pipfile*
 
 chmod +x files/connection-details
-cp files/connection-details ${ROOTFS_DIR}/usr/local/bin/connection-details
+cp files/connection-details "${ROOTFS_DIR}"/usr/local/bin/connection-details
