3ba952273085688c39ee4b1212cb67ae3f558b60
Mayank <mayankchhabra9@gmail.com>
2020-07-29 15:31:22 +0530

    Install Umbrel master for now

--- stage2/05-docker-compose/01-run.sh
+++ stage2/05-docker-compose/01-run.sh
@@ -6,7 +6,13 @@ echo "Installing docker-compose from pip3, and also setting up the box folder st
 on_chroot << EOF
 pip3 install docker-compose
 cd /home/${FIRST_USER_NAME}
-wget -qO- "https://raw.githubusercontent.com/getumbrel/umbrel/v${UMBREL_VERSION}/install-box.sh" | sh
+
+git init
+git remote add origin https://github.com/getumbrel/umbrel.git
+git fetch --all
+git checkout origin/master
+git reset --hard origin/master
+
 chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 EOF
 
