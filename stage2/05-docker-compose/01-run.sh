abd1992ff80fd8538e1b56cf80ef77f1227525f5
Mayank <mayankchhabra9@gmail.com>
2020-08-07 17:11:48 +0530

    Install systemd Umbrel

--- stage2/05-docker-compose/01-run.sh
+++ stage2/05-docker-compose/01-run.sh
@@ -11,21 +11,6 @@ curl -L https://github.com/getumbrel/umbrel/archive/v${UMBREL_VERSION}.tar.gz | 
 chown -R ${FIRST_USER_NAME}:${FIRST_USER_NAME} /home/${FIRST_USER_NAME}
 EOF
 
-# Docker compose service
-on_chroot << EOF
-mkdir -p /etc/init.d
-mkdir -p /etc/rc2.d
-mkdir -p /etc/rc3.d
-mkdir -p /etc/rc4.d
-mkdir -p /etc/rc5.d
-mkdir -p /etc/rc0.d
-mkdir -p /etc/rc1.d
-mkdir -p /etc/rc6.d
-EOF
-
-echo "Copying the umbrel service to rootfs (etc/init.d)"
-install -m 755 files/umbrel "${ROOTFS_DIR}"/etc/init.d/umbrel
-
 echo "Pulling Docker images required to run Umbrel services"
 
 wget -q "https://raw.githubusercontent.com/getumbrel/umbrel/v${UMBREL_VERSION}/docker-compose.yml"
