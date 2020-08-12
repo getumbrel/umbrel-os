054c3d7cf3a17ac247b7a687cc3ca945188e019f
Mayank <mayankchhabra9@gmail.com>
2020-08-12 13:38:49 +0530

    Install Umbrel from runner to image

--- stage2/03-install-umbrel/01-run.sh
+++ stage2/03-install-umbrel/01-run.sh
@@ -23,22 +23,45 @@ EOF
 echo "Installing Umbrel..."
 echo
 
-on_chroot << EOF
-mkdir /home/${FIRST_USER_NAME}/umbrel
-cd /home/${FIRST_USER_NAME}/umbrel
+mkdir /umbrel
+cd /umbrel
 curl -L https://github.com/getumbrel/umbrel/archive/v${UMBREL_VERSION}.tar.gz | tar -xz --strip-components=1
-UMBREL_SYSTEMD_SERVICES=$(ls /home/${FIRST_USER_NAME}/umbrel/scripts/umbrel-os/services/*.service)
+echo "Debugging dir:"
+ls 
+cd /scripts/umbrel-os/services
+echo "Debugging services:"
+ls
+UMBREL_SYSTEMD_SERVICES=$(ls *.service)
+echo "Services:"
+echo $UMBREL_SYSTEMD_SERVICES
 for service in $UMBREL_SYSTEMD_SERVICES; do
+    echo "Replacing /home/umbrel in ${service} with /home/${FIRST_USER_NAME}"
     sed -i -e "s/\/home\/umbrel/\/home\/${FIRST_USER_NAME}/g" "${service}"
-    cp "${service}" "/etc/systemd/system/${service}"
-    systemctl enable "${service}"
-done
+    echo "Installing  ${service} at ${ROOTFS_DIR}/etc/systemd/system/${service}"
+    install -m 644 "${service}"   "${ROOTFS_DIR}/etc/systemd/system/${service}"
+    echo "Enabling ${service}"
+    on_chroot << EOF
+systemctl enable "${service}"
 EOF
+done
+
+echo "Creating ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel"
+mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel"
+
+echo "Copying /umbrel to ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel"
+rsync -avPHSX /umbrel "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
+
+echo "Debugging ROOTFS home"
+ls ${ROOTFS_DIR}/home/${FIRST_USER_NAME}
+echo "Debugging ROOTFS umbrel"
+ls ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/umbrel
+echo "Debugging ROOTFS systemd"
+ls ${ROOTFS_DIR}/etc/systemd/system/
 
 # Bundle Umbrel's Docker images
 echo "Pulling Umbrel's Docker images..."
 echo
-wget -q "https://raw.githubusercontent.com/getumbrel/umbrel/v${UMBREL_VERSION}/docker-compose.yml"
+cd /umbrel
 IMAGES=$(grep '^\s*image' docker-compose.yml | sed 's/image://' | sed 's/\"//g' | sed '/^$/d;s/[[:blank:]]//g' | sort | uniq)
 echo
 echo "Images to bundle: $IMAGES"
