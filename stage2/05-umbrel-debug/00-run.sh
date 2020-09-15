ac9552419e618880ced1e3ca6e61f2447da6eca3
Aaron Dewes <aaron.dewes@web.de>
2020-09-15 16:42:33 +0200

    Update 00-run.sh

--- stage2/05-umbrel-debug/00-run.sh
+++ stage2/05-umbrel-debug/00-run.sh
@@ -2,5 +2,5 @@
 
 echo "Installing umbrel-debug"
 
-install -m 755 files/umbrel-debug "${ROOTFS_DIR}"/usr/local/bin/umbrel-debug
-sed -i -e "s/\/home\/umbrel/\/home\/${FIRST_USER_NAME}/g" /usr/local/bin/umbrel-debug
+install -m 755 files/umbrel-debug "${ROOTFS_DIR}"/usr/bin/umbrel-debug
+sed -i -e "s/\/home\/umbrel/\/home\/${FIRST_USER_NAME}/g" "${ROOTFS_DIR}"/usr/bin/umbrel-debug
