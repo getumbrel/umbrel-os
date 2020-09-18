927581074dfb7146a9f48b1b30787c117b168046
Raphael Nestler <raphael.nestler@gmail.com>
2020-09-18 15:40:33 +0200

    Only disable wifi if WPA_COUNTRY is not set (#416)
    * Only disable wifi if WPA_COUNTRY is not set
    
    * Use '-n' instead of '-v' to test if WPA_COUNTRY is set
    
    Apparently -v doesn't work in all shells.

--- stage2/01-net-tweaks/01-run.sh
+++ stage2/01-net-tweaks/01-run.sh
@@ -27,7 +27,12 @@ network={
 EOL
 fi
 
-# Disable wifi on 5GHz models
+# Disable wifi on 5GHz models if WPA_COUNTRY is not set
 mkdir -p "${ROOTFS_DIR}/var/lib/systemd/rfkill/"
-echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmc:wlan"
-echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmc:wlan"
+if [ -n "$WPA_COUNTRY" ]; then
+    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
+    echo 0 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
+else
+    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
+    echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
+fi
